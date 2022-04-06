4d968e5afc5d8f6c4ab51a0eb354fba7dc40b7e5
ERieger <e.rieger04@gmail.com>
2022-04-06 22:59:38 +0930

    Basic navbar styling

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
 
 <head>
     <title>Crash Data Web Interface</title>
-    <link rel=stylesheet href="./index.css">
+    <link rel=stylesheet href="index.css">
 </head>
 
 <body>
     <div class="page">
         <div class="nav">
             <h1>Road Crash Data Entry</h1>
+            <img src="./images/upload.png" alt="Upload">
         </div>
     </div>
     <?php
