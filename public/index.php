ebdba0c4206a7d0f288ee6f7fe2412f8d73fc023
ERieger <e.rieger04@gmail.com>
2022-04-06 13:17:37 +0930

    Basic page style

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Crash Data Web Interface</title>
+    <link rel=stylesheet href="./index.css">
+</head>
+
+<body>
+    <div class="page">
+        <div class="nav">
+            <h1>Road Crash Data Entry</h1>
+        </div>
+    </div>
+    <?php
+    include('connect.php');
+    ?>
+</body>
+
+</html>
