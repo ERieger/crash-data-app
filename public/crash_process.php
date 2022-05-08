ebbc1eebadf5096533d64b5a133358e64a312d76
ERieger <e.rieger04@gmail.com>
2022-05-08 15:38:25 +0930

    Comment html form logic

--- public/crash_process.php
+++ public/crash_process.php
@@ -1,5 +1,6 @@
 <?php
-include('connect.php');
+include('connect.php'); // Connect to database
+// Get the values form the form
 $day = $_POST["day"];
 $month = $_POST["month"];
 $year = $_POST["year"];
@@ -15,13 +16,15 @@ $areaSpeed = $_POST["areaSpeed"];
 $posType = $_POST["posType"];
 $crashType = $_POST["crashType"];
 
+// Handel Checkboxes
 if(isset($_POST['DUIInvolved']) && 
-   $_POST['DUIInvolved'] == 'true') {
-    $DUIInvolved = 1;
-} else {
-    $DUIInvolved = 0;
+   $_POST['DUIInvolved'] == 'true') { // If checbox checked
+    $DUIInvolved = 1; // Set variable to true
+} else { // Not checked
+    $DUIInvolved = 0; // Set to false
 }
 
+// See above comments
 if(isset($_POST['drugsInvolved']) && 
    $_POST['drugsInvolved'] == 'true') {
     $drugsInvolved = 1;
@@ -30,28 +33,26 @@ if(isset($_POST['drugsInvolved']) &&
 }
 
 function validate($data) {
-    $data = trim($data);
+    $data = trim($data); // Trim whitespace
+    $data = htmlspecialchars($data); // Prevent SQL data injection by removing special characters
     return $data;    
 }
 
+// SQL statement for data insert
 $SQL = "INSERT INTO `c_crash_data`(`day`, `month`, `year`, `time`, `locid`, `acclocx`, `acclocy`, `total_cas`, `total_fats`, `total_si`, `total_mi`, `area_speed`, `pos_type`, `crash_type`, `dui`, `drugs`) 
 VALUES('$day', '$month', $year, '$time', $location, $accloc_x, $accloc_y, $totalCas, $totalFat, $totalSI, $totalMI, $areaSpeed, $posType, $crashType, $DUIInvolved, $drugsInvolved)";
 
+// Add records
 if(mysqli_query($conn, $SQL)){
-
-    echo "Records added successfully.";
-
+    echo "Records added successfully."; // Acknowledge successful entry
 } else{
-
-    echo "ERROR: Could not able to execute $SQL. " . mysqli_error($conn);
-
+    echo "ERROR: Could not able to execute $SQL. " . mysqli_error($conn); // Log error
 }
 
- 
-
-// Close connection
+// Close connection to database
 mysqli_close($conn);
 
+// Retrun to index page
 header("Location: index.php");
 exit;
 ?>
